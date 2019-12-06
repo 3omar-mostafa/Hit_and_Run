d03e71421e18bdfa446072521fce02ae16b2dabf
hagerali99 <56045136+hagerali99@users.noreply.github.com>
2019-12-06 15:41:34 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -6,30 +6,21 @@ if not exist masm.exe echo Please download it and make sure it is masm.exe
 if not exist link.exe echo Microsoft (R) Segmented Executable Linker is Not Found
 if not exist link.exe echo Please download it and make sure it is link.exe
 
-if exist draw.obj erase draw.obj
-if exist welcome.obj erase welcome.obj
-if exist main.obj erase main.obj
+if exist graphics.obj erase graphics.obj
 if exist inout.obj erase inout.obj
 
 if exist game.exe erase game.exe
 
-masm draw.asm /z /Zi /Zd /v    > draw.log ,%draw ;
-If not exist draw.obj echo Assembling Failed , Check draw.log for errors
-If not exist draw.obj goto end
+masm graphics.asm /z /Zi /Zd /v    > graphics.log ,%graphics ;
+If not exist graphics.obj echo Assembling Failed , Check graphics.log for errors
+If not exist graphics.obj goto end
 
 masm inout.asm /z /Zi /Zd /v    > inout.log ,%inout ;
 If not exist inout.obj echo Assembling Failed , Check inout.log for errors
 If not exist inout.obj goto end
 
-masm main.asm /z /Zi /Zd /v    > main.log ,%main ;
-If not exist main.obj echo Assembling Failed , Check main.log for errors
-If not exist main.obj goto end
 
-masm welcome.asm /z /Zi /Zd /v > welcome.log ,%welcome ;
-If not exist welcome.obj echo Assembling Failed , Check welcome.log for errors
-If not exist welcome.obj goto end
-
-link  main.obj inout.obj draw.obj welcome.obj > link.log ,game.exe,nul;
+link  graphics.obj inout.obj > link.log ,game.exe,nul;
 If not exist game.exe echo Linking Failed , Check link.log for errors
 If not exist game.exe goto end
 
